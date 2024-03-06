1afd1e1547dcdfeb0ac6a8e00863cf67a6992221
Elijah <eachala5@gmail.com>
2024-03-05 17:05:34 -0800

    009

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,11 +11,16 @@ return (binary_tree_height1(tree, 0));
 /**
  * binary_tree_height1 - finds height of a binary tree
  * @tree: ptr to root
+ * @h: height
  * Return: the height
  */
 size_t binary_tree_height1(const binary_tree_t *tree, size_t h)
 {
 size_t lh, rh;
+if (tree == NULL && h == 0)
+{
+return (0);
+}
 if (tree != NULL)
 {
 lh = binary_tree_height1(tree->left, h + 1);
